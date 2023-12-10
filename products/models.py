bf51ef25f9ea43a3d7a45e5b5687a0fe3acd6585
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-12-10 18:02:56 +0000

    Add products app

---
+++ products/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class ProductVariant(models.Model):
+    CLOTHING_SIZE_CHOICES = [
+        ('XS', 'Extra Small'),
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('XL', 'Extra Large'),
+        ('XXL', 'Double Extra Large'),
+        ('3XL', 'Triple Extra Large'),
+    ]
+
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('C', 'Child'),
+    ]
+
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    clothing_size = models.CharField(max_length=10,choices=CLOTHING_SIZE_CHOICES, null=True, blank=True)
+    men_shoe_size = models.CharField(max_length=10, null=True, blank=True)
+    women_shoe_size = models.CharField(max_length=10, null=True, blank=True)
+    child_shoe_size = models.CharField(max_length=10, null=True, blank=True)
+    color = models.CharField(max_length=50, null=True, blank=True)
+    stock_quantity = models.PositiveIntegerField(default=0)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.product.name} - Clothing Size: {self.clothing_size}, Men Shoe Size: {self.men_shoe_size}, Women Shoe Size: {self.women_shoe_size}, Child Shoe Size: {self.child_shoe_size}, Color: {self.color}'
+
