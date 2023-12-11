6bdbacd6d905cbaac8b09ec00a08e39ca9539f6f
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-12-11 10:53:47 +0000

    Add context.py

--- base/urls.py
+++ base/urls.py
@@ -19,14 +19,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 # from .views import handler404
 
-# handler404 = 'boutique_ado.views.handler404'
+# handler404 = 'base.views.handler404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
-    # path('bag/', include('bag.urls')),
+    path('bag/', include('bag.urls')),
     # path('checkout/', include('checkout.urls')),
     # path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
