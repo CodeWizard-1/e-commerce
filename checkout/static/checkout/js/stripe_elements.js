ac03182d34b8834fd71b7795866cc53651ef3b44
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-12-12 11:50:28 +0000

    Update models on checkout app

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -28,6 +28,7 @@ var style = {
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
+// Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
@@ -43,6 +44,7 @@ card.addEventListener('change', function (event) {
     }
 });
 
+// Handle form submit
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
@@ -111,7 +113,7 @@ form.addEventListener('submit', function(ev) {
             }
         });
     }).fail(function () {
-
+        // just reload the page, the error will be in django messages
         location.reload();
     })
 });
