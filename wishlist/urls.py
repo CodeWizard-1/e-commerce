09a0acc4b1f77f99d997a762cd3a955875f1a3ea
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-12-13 21:12:55 +0000

    Update wishlist page

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
 from .views import WishlistView, add_to_wishlist, remove_from_wishlist
 
 urlpatterns = [
-    path('', views.WishlistView.as_view(), name='wishlist'),
-    path('add-to-wishlist/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
-    path('remove-from-wishlist/<int:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+    path('', WishlistView.as_view(), name='wishlist'),
+    path('add-to-wishlist/<int:product_id>/', add_to_wishlist, name='add_to_wishlist'),
+    path('remove-from-wishlist/<int:product_id>/', remove_from_wishlist, name='remove_from_wishlist'),
 ]
+
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
@@ -6,28 +6,19 @@ from .models import Wishlist, Product
 
 class WishlistView(View):
     def get(self, request):
-
         wishlist, created = Wishlist.objects.get_or_create(user=request.user)
         return render(request, 'wishlist.html', {'wishlist': wishlist})
 
 @login_required
 def add_to_wishlist(request, product_id):
-    try:
-        product = Product.objects.get(pk=product_id)
-    except Product.DoesNotExist:
-        raise Http404("Product not found")
-
+    product = get_object_or_404(Product, pk=product_id)
     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
     wishlist.products.add(product)
     return redirect('wishlist')
 
 @login_required
 def remove_from_wishlist(request, product_id):
-    try:
-        product = Product.objects.get(pk=product_id)
-    except Product.DoesNotExist:
-        raise Http404("Product not found")
-
+    product = get_object_or_404(Product, pk=product_id)
     wishlist = Wishlist.objects.get(user=request.user)
     wishlist.products.remove(product)
     return redirect('wishlist')
