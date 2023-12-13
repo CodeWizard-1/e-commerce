f884975205ca16762ef980f55f454700039540e4
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-12-13 12:16:21 +0000

    Add 404.html page

--- base/settings.py
+++ base/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = ['sportswear-ebbdbb6bb6ec.herokuapp.com','8000-codewizard1-ecommerce-rkjafijsu1w.ws-eu106.gitpod.io']
 
 
@@ -231,7 +231,7 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'sportswear@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    # EMAIL_USE_SSL = True
+    EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
