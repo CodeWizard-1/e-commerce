bbdf398fe77b4ba766f705afda81f178b4f3cf29
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-12-14 12:06:22 +0000

    Add class reviews and new firlds to models products

--- products/models.py
+++ products/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+from django.contrib.auth.models import User
+from profiles.models import UserProfile
 
 class Category(models.Model):
 
@@ -28,6 +33,30 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    brand = models.ForeignKey("Brand", null=True, blank=True, on_delete=models.SET_NULL)
+    on_sale = models.BooleanField(default=False)
+    discount = models.IntegerField(
+        default=10,
+        help_text="Discount in Percentage",
+        verbose_name="Discount If Product On Sale",
+    )
+    discounted_price = models.IntegerField(null=True)
+    created_on = models.DateField(default=timezone.now)
+    average_rating = models.DecimalField(
+    max_digits=6, decimal_places=2, null=True, blank=True
+    )
+
+    def clean(self):
+        if self.price < 0:
+            raise ValidationError("Price cannot be negative.")
+
+    @property
+    def discounted_price(self):
+        return ((self.price) * (self.discount)) / 100
+
+    @property
+    def sale_price(self):
+        return (self.price) - (self.discounted_price)
 
     def __str__(self):
         return self.name
@@ -61,3 +90,34 @@ class ProductVariant(models.Model):
     def __str__(self):
         return f'{self.product.name} - Clothing Size: {self.clothing_size}, Men Shoe Size: {self.men_shoe_size}, Women Shoe Size: {self.women_shoe_size}, Child Shoe Size: {self.child_shoe_size}, Color: {self.color}'
 
+
+class Brand(models.Model):
+    class Meta:
+        verbose_name_plural = "Brands"
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Reviews(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=1500, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["user", "product"], name="reviews_per_user")
+        ]
+
+    def __str__(self):
+        return self.title
