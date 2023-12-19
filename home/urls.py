6737476d9e8e2aa8d4f97f0ae17c01ce04435520
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-12-19 09:53:50 +0000

    Add private_policy page

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path("privacy_policy", views.privacy_policy, name="privacy_policy"),
+    path("contact", views.contact, name="contact"),
 ]
